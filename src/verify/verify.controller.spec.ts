bfbe4b50d048b95dd622c68fc2aad703ab59ccdb
Chris Anatalio <chris.anatalio@stellar.org>
2025-01-07 23:25:34 -0800

    Update README (#1)
    * Update README
    
    * Fix workflow
    
    * Fix workflow
    
    * Fix workflow
    
    * Fix tests
    
    * Add coverage
    
    * Add minimum code coverage
    
    * Fix config

--- src/verify/verify.controller.spec.ts
+++ src/verify/verify.controller.spec.ts
@@ -16,7 +16,7 @@ describe('VerifyController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getHello()).toBeNull();
     });
   });
 });
